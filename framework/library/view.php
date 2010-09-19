4fa62be2a06cb083875d74726b9464328b9fc8d2
Sudhanshu Raheja <sudhanshu.raheja@vxtindia.com>
2010-09-19 14:02:18 +0000

    Added chref for javascript

--- framework/library/view.php
+++ framework/library/view.php
@@ -185,8 +185,11 @@
 			$content .= "
 				<script type='text/javascript'>
 					var Generatrix = {
-						basepath: '" . chref('') . "',
+						basepath: '" . href('') . "',
+						use_cdn: " . USE_CDN . ",
+						cbasepath: '" . chref('') . "',
 						href: function(path) { return this.basepath + path; },
+						chref: function(path) { if(use_cdn) { return this.cbasepath + this.href(path) } else { href(path) } },
 						loading: function(where) { $(where).html(\"<img src='\" + this.href('/images/gears.gif') + \"' />\"); },
 						timestamp: function() { var d = new Date(); return d.getTime() / 1000; },
 						rand: function(max) { return Math.ceil(Math.random() * max); }
