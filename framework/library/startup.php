cb2f39c4c8e5ace301538f27333415e913b1c838
Sudhanshu Raheja <sudhanshu.raheja@vxtindia.com>
2010-12-01 18:49:57 +0530

    Added a fix to show 404 page on chrome

--- framework/library/startup.php
+++ framework/library/startup.php
@@ -26,6 +26,11 @@
 		} else {
 			header("HTTP/1.1 404 Not Found");
 			echo "<div style='width: 600px; margin: 150px auto; text-align: center; background-color: #F7F7F7; border: 10px solid #EEEEEE; padding: 40px 0px; font-family: Georgia; Arial, sans-serif;'>This page does not exist</div>";
+			// To show error page on Chrome, text needs to be bigger than 512 bytes, so just repeating
+			echo "<div style='display: none;'>";
+			echo "<div style='width: 600px; margin: 150px auto; text-align: center; background-color: #F7F7F7; border: 10px solid #EEEEEE; padding: 40px 0px; font-family: Georgia; Arial, sans-serif;'>Chrome does not show your error page if the content size is lesser than 512 bytes, hence we have had to take this extreme step.</div>";
+			echo "<div style='width: 600px; margin: 150px auto; text-align: center; background-color: #F7F7F7; border: 10px solid #EEEEEE; padding: 40px 0px; font-family: Georgia; Arial, sans-serif;'>This page really does not exist</div>";
+			echo "</div>";
 		}
 	}
 
